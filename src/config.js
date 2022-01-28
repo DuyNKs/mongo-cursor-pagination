83c6b9a73a6ab927001390c433dd1c89dc620216
NK Duy <me@nkduy.info>
2022-01-28 10:01:33 +0700

    Add limit max page to 10000

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   /**
    * {Number} The maximum limit (page size).
    */
-  MAX_LIMIT: 300,
+  MAX_LIMIT: 10000,
 
   /**
    * {Number} The default limit (page size), if none is specified.
